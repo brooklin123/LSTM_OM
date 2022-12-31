780bc782c1b8b46c592f8db993305fd05f76503f
brooklin123 <count2841@gmail.com>
2022-12-31 20:29:03 +0800

    feat: mae

--- LSTM.py
+++ LSTM.py
@@ -8,6 +8,7 @@ from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 
 
 # 產生 (X, Y) 資料集, Y 是下一期的乘客數
@@ -62,11 +63,16 @@ testPredict = scaler.inverse_transform(testPredict)
 testY = scaler.inverse_transform([testY])
 
 # calculate 均方根誤差(root mean squared error)
+trainmae = mean_absolute_error((trainY[0], trainPredict[:,0]))
+testmae = testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
 
+print('Train Score: %.2f MAE' % (trainmae))
+testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+print('Test Score: %.2f MAE' % (testmae))
 # 畫訓練資料趨勢圖
 # shift train predictions for plotting
 trainPredictPlot = numpy.empty_like(dataset)
@@ -81,7 +87,9 @@ testPredictPlot[len(trainPredict)+(look_back*2)+1:len(dataset)-1, :] = testPredi
 
 # 畫原始資料趨勢圖
 # plot baseline and predictions
-plt.plot(scaler.inverse_transform(dataset))
+plt.plot(scaler.inverse_transform(dataset)) #將轉換的資料還原
 plt.plot(trainPredictPlot)
 plt.plot(testPredictPlot)
+plt.title('Airline passenger traffic')
+plt.grid() # 有框線
 plt.show()
